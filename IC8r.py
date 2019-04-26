9d7150f4399ff65ba9d3bf938d28432a14d63068
Alize Casas <X00355313@wcc.dom>
2019-04-26 12:36:58 -0500

    Fixed the program that reads the file

--- IC8r.py
+++ IC8r.py
@@ -3,13 +3,16 @@ def main():
     try:
         infile = open(filename, 'r')
         total = 0
+        count = 0
         for x in infile:
             amount = float(x)
             total += amount
+            count+=1
         infile = open(filename, 'r')
         numbers = infile.read()
         print('The numbers in the file are:', numbers)
         infile.close()
+        print('There are',count,'numbers total')
         print('The sum of the numbers is:',total)
     except FileNotFoundError:
         print('File cannot be found')
