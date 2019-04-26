a11bd0bf2e0470ba442f363b3c67749ea0d80398
Alize Casas <X00355313@wcc.dom>
2019-04-26 12:37:44 -0500

    Fixed program that writes on the file

--- IC8w.py
+++ IC8w.py
@@ -1,14 +1,15 @@
 import random
 def main():
-    number_file = open('random', 'w')
+    try:
+        number_file = open('random', 'w')
 
-    random_amount = int(input('Please enter how many random numbers the file will hold: '))
+        random_amount = int(input('Please enter how many random numbers the file will hold: '))
 
-    for x in range(random_amount):
-        number = random.randint(1,500)
-        print(number)
-        number_file.write(str(number) + '\n')
-    number_file.close()
-
+        for x in range(random_amount):
+            number = random.randint(1,500)
+            number_file.write(str(number) + '\n')
+        number_file.close()
+    except ValueError:
+        print('Error: you must enter a number')
 
 main()
