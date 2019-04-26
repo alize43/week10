835afcf7d1387ec7cb9d81cbc3c7dfaaf456b2e7
Alize Casas <X00355313@wcc.dom>
2019-04-26 13:04:07 -0500

    Split into two functions

--- IC8w.py
+++ IC8w.py
@@ -1,15 +1,21 @@
 import random
+def random_number():
+    number = random.randint(1, 500)
+    return number
 def main():
     try:
+        random_amount = int(input('Please enter how many random numbers the file will hold: '))
         number_file = open('random', 'w')
 
-        random_amount = int(input('Please enter how many random numbers the file will hold: '))
-
-        for x in range(random_amount):
-            number = random.randint(1,500)
+        for x in range(1, 1 + random_amount):
+            number = random_number()
             number_file.write(str(number) + '\n')
         number_file.close()
     except ValueError:
         print('Error: you must enter a number')
+
+
+
+
 
 main()
